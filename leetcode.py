402455d739ae925c84bd70cd128170848aa350b2
AbenezerAngamo <aangamo@aimsammi.org>
2025-05-22 23:32:57 +0000

    add isPalindrom implementation

--- leetcode.py
+++ leetcode.py
@@ -4,11 +4,18 @@ leetcode.py
 
 class LeetCode:
     
-    def twoSum(self, nums: List[int], target: int) -> List[int]:        
+    def twoSum(self, nums: list[int], target: int) -> list[int]:        
         i,j=0,len(nums)-1       
         hash = {}
 
         for i in range(len(nums)):
             if (target-nums[i]) in hash:
                 return i, hash[(target-nums[i])]
-            hash[nums[i]] = i        
+            hash[nums[i]] = i            
+
+    def isPalindrome(self, x: int) -> bool:                
+        x_str = str(x)       
+        if x_str == x_str[::-1]:
+            return True
+        else:
+            return False
